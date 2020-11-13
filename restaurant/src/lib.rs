269890e51a64287932a53baf561d6f5ac30644cc
Tim Robinson <tim@timjrobinson.com>
2020-11-13 17:41:46 +1000

    Chapter 7. Packages,Crates,Modules

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,70 @@
+mod front_of_house;
+mod back_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+
+//         fn seat_at_table() {}
+//     }
+
+//     mod serving {
+//         fn take_order() {}
+
+//         fn serve_order() {}
+
+//         fn take_payment() {}
+//     }
+// }
+
+
+// mod back_of_house {
+//     pub struct Breakfast {
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
+
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast {
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+
+//         pub fn get_fruit(&self) -> &String {
+//             return &self.seasonal_fruit;
+//         }
+//     }
+// }
+
+// use crate::front_of_house::hosting;
+
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // Relative path
+//     front_of_house::hosting::add_to_waitlist();
+
+//     // With use
+//     hosting::add_to_waitlist();
+
+//     // Order a breakfast in the summer with Rye toast
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     // Change our mind about what bread we'd like
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
+//     let fruit = meal.get_fruit();
+//     println!("This meal comes with {}", fruit);
+
+// }
